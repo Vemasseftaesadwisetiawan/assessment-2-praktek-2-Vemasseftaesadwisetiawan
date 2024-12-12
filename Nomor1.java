853a6914fc4fe35382ce5bcf6835bc58c69b1fac
Vemas seftaesa dwi setiawan <vmasstwn@gmail.com>
2024-12-12 10:59:55 +0700

    Update Nomor1.java

--- Nomor1.java
+++ Nomor1.java
@@ -2,14 +2,28 @@ import java.util.Scanner;
 
 public class Nomor1 {
 
+    public static void printFizzBuzz(int n) {
+        for (int i = 0; i <= n; i++) {
+            if (i % 3 == 0 && i % 5 == 0) {
+                System.out.println("FizzBuzz");
+            } else if (i % 3 == 0) {
+                System.out.println("Fizz");
+            } else if (i % 5 == 0) {
+                System.out.println("Buzz");
+            } else {
+                System.out.println(i);
+            }
+        }
+    }
+
     public static void main(String[] args){
         Scanner input = new Scanner(System.in);
+        System.out.print("Masukkan nilai N: ");
         int N = input.nextInt();
-        /* Pekerjaan anda mulai dari sini */
 
-        /* Pekerjaan anda berakhir sini */
-
+       
+        printFizzBuzz(N);
+        
         System.out.println(Nomor1.class.getDeclaredMethods().length); 
     }
-    
 }
